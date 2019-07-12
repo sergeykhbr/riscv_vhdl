5383c68e6675ebaab5468f172bbc5a7b24f550e7
sergeykhbr <sergeykhbr@gmail.com>
2019-07-12 17:11:47 +0300

    [*] continue developing icache func model

--- debugger/src/cpu_fnc_plugin/icache_func.cpp
+++ debugger/src/cpu_fnc_plugin/icache_func.cpp
@@ -19,95 +19,6 @@
 
 namespace debugger {
 
-static const int BUS_ADDR_WIDTH = 32;
-static const int OFFSET_WIDTH = 5;   // [4:0]   offset: 32 bytes per line
-static const int INDEX_WIDTH  = 9;   // [13:5]  index: 16 KB per way
-static const int TAG_WIDTH    = 18;  // [31:14] tag
-
-class Mem16 {
- public:
-    Mem16(uint64_t sz) {
-        size_ = sz;
-        mem_ = new uint16_t[size_];
-    }
-    ~Mem16() {
-        delete [] mem_;
-    }
-    void read(uint64_t addr, uint16_t *data) {
-        addr %= size_;
-        *data = mem_[addr];
-    }
-    void write(uint64_t addr, uint16_t data) {
-        addr %= size_;
-        mem_[addr] = data;
-    }
-   
- protected:
-    uint16_t *mem_;
-    uint64_t size_;
-};
-
-class MemTag {
- public:
-    MemTag(uint64_t sz) {
-        size_ = sz;
-        // [21:4] tag value
-        // [3:0]  qword is valid
-        mem_ = new uint32_t[size_];
-    }
-    ~MemTag() {
-        delete [] mem_;
-    }
-    void read(uint64_t addr, uint32_t *data) {
-        addr %= size_;
-        *data = mem_[addr];
-    }
-    void write(uint64_t addr, uint32_t data) {
-        addr %= size_;
-        mem_[addr] = data;
-    }
-   
- protected:
-    uint32_t *mem_;
-    uint64_t size_;
-};
-
-class MemWay {
-    MemWay() {
-        totsz_ = (1 << 14);
-        linesz_ = 32;
-        linetot_ = totsz_ / linesz_;
-        memtag_ = new MemTag(linetot_);
-        for (uint64_t i = 0; i < linesz_ / sizeof(uint16_t); i++) {
-            mem16_[i] = new Mem16(linetot_);
-        }
-    }
-    ~MemWay() {
-        for (uint64_t i = 0; i < linesz_ / sizeof(uint16_t); i++) {
-            delete mem16_[i];
-        }
-        delete memtag_;
-    }
-    void read(uint64_t addr, uint32_t *data) {
-        //addr %= size_;
-        //*data = mem_[addr];
-    }
-    void write(uint64_t addr, uint32_t data) {
-        //addr %= size_;
-        //mem_[addr] = data;
-    }
-   
- protected:
-    uint64_t totsz_;
-    uint64_t linesz_;
-    uint64_t linetot_;
-
-    MemTag *memtag_;
-    Mem16 *mem16_[16];
-};
-
-MemWay memway_;
-
 ICacheFunctional::ICacheFunctional(const char *name) : IService(name),
     ICommand(name, 0) {
     registerInterface(static_cast<IMemoryOperation *>(this));
@@ -146,77 +57,164 @@ void ICacheFunctional::predeleteService() {
     }
 }
 
-struct CacheLineData {
-    uint64_t tag;
-    Reg16Type mem16[16];
-    uint32_t valid;     // 16-bits bit field
-};
-
-class WayMemType {
- public:
-    WayMemType() {
-    }
-    ~WayMemType() {
-    }
-
-    void getValue(uint32_t index, CacheLineData *data) {
-    }
-};
-
 ETransStatus ICacheFunctional::b_transport(Axi4TransactionType *trans) {
-    Axi4TransactionType tr;
-    for (unsigned i = 0; i < 2; i++) {
-        req_.adr[i].tag = getAdrTag(trans->addr + i*sizeof(uint16_t));
-        req_.adr[i].index = getAdrIndex(trans->addr + i*sizeof(uint16_t));
-        req_.adr[i].offset = getAdrOffset(trans->addr + i*sizeof(uint16_t));
-        req_.adr[i].hit = false;
+    uint32_t rcache;
+
+    int hit = getCachedValue(trans->addr, rcache);
+    if (hit == HIT_BOTH) {
+        trans->rpayload.b64[0] = rcache;
+        return TRANS_OK;
     }
 
-    bool even_odd_swap = false;
-    if (trans->addr & 0x2) {
-        even_odd_swap = true;
-    } else {
-        even_odd_swap = false;
-    }
-
-    CacheLineData data;
-    WayMemType memWay[4];
-    Reg64Type resp;
-    unsigned hit_way[2] = {~0u, ~0u};
-    for (unsigned i = 0; i < 2; i++) {
-        for (unsigned n = 0; n < ways_.to_uint32(); n++) {
-            memWay[n].getValue(req_.adr[i].index, &data);
-            if (data.tag == req_.adr[i].tag) {
-                hit_way[i] = n;
-                resp.buf16[i] = data.mem16[req_.adr[i].offset >> 1].word;
-            }
+    int lru = 0;
+    WayMemType *waysel;
+    if ((hit & HIT_WORD0) == 0) {
+        if (getAdrOddEven(trans->addr) == WAY_EVEN) {
+            waysel = &wayEven_[lru];
+        } else {
+            waysel = &wayOdd_[lru];
         }
+        readLine(trans->addr, waysel);
     }
 
-    for (unsigned i = 0; i < 2; i++) {
-        if (hit_way[i] != ~0u) {
-            continue;
+    uint64_t overlay_addr = trans->addr + 2;
+    if (getAdrLine(trans->addr) != getAdrLine(overlay_addr)
+        && (hit & HIT_WORD1) == 0) {
+        if (getAdrOddEven(trans->addr) == WAY_EVEN) {
+            waysel = &wayOdd_[lru];
+        } else {
+            waysel = &wayEven_[lru];
         }
+        readLine(overlay_addr, waysel);
+    }
 
+    hit = getCachedValue(trans->addr, rcache);
+    trans->rpayload.b64[0] = rcache;
+    if (hit != HIT_BOTH) {
+        RISCV_error("Something wrong");
     }
     return TRANS_OK;
 }
 
+int ICacheFunctional::getCachedValue(uint64_t addr, uint32_t &rdata) {
+    uint64_t line_adr = getAdrLine(addr);
+    uint64_t tag = getAdrTag(addr);
+
+    EWays useWay = static_cast<EWays>(getAdrOddEven(addr));
+    bool useOverlay = false;
+    if ((addr & 0x1E) == 0x1E) {
+        useOverlay = true;
+    }
+
+    uint64_t swapped_index[WAY_SubNum];
+    uint64_t swapped_offset[WAY_SubNum];
+    if (useWay == WAY_EVEN) {
+        swapped_index[WAY_EVEN]  = getAdrIndex(addr);
+        swapped_offset[WAY_EVEN] = getAdrOffset(addr);
+        swapped_index[WAY_ODD]   = (getAdrIndex(addr) + 1) % LINES_PER_WAY;
+        swapped_offset[WAY_ODD]  = 0;
+    } else {
+        swapped_index[WAY_EVEN]  = (getAdrIndex(addr) + 1) % LINES_PER_WAY;
+        swapped_offset[WAY_EVEN] = 0;
+        swapped_index[WAY_ODD]   = getAdrIndex(addr);
+        swapped_offset[WAY_ODD]  = getAdrOffset(addr);
+    }
+
+    uint64_t cachetag[WAY_SubNum];
+    uint32_t cacheval[WAY_SubNum];
+    unsigned hit = 0;
+    rdata = 0;
+
+    for (unsigned n = 0; n < ICACHE_WAYS; n++) {
+        wayEven_[n].readTagValue(swapped_index[WAY_EVEN],
+                                 swapped_offset[WAY_EVEN],
+                                 cachetag[WAY_EVEN],
+                                 cacheval[WAY_EVEN]);
+
+        wayOdd_[n].readTagValue(swapped_index[WAY_ODD],
+                                swapped_offset[WAY_ODD],
+                                cachetag[WAY_ODD],
+                                cacheval[WAY_ODD]);
+
+
+        if (useWay == WAY_EVEN) {
+            if (tag == cachetag[WAY_EVEN]) {
+                hit |= HIT_WORD0;
+                rdata = cacheval[WAY_EVEN];
+                if (!useOverlay) {
+                    hit |= HIT_WORD1;
+                } else if (tag == cachetag[WAY_ODD]) {
+                    hit |= HIT_WORD1;
+                    rdata &= ~0xFFFF0000;
+                    rdata |= (cacheval[WAY_ODD] << 16);
+                }
+            }
+        } else {
+            if (tag == cachetag[WAY_ODD]) {
+                hit |= HIT_WORD0;
+                rdata = cacheval[WAY_ODD];
+                if (!useOverlay) {
+                    hit |= HIT_WORD1;
+                } else if (tag == cachetag[WAY_EVEN]) {
+                    hit |= HIT_WORD1;
+                    rdata &= ~0xFFFF0000;
+                    rdata |= (cacheval[WAY_EVEN] << 16);
+                }
+            }
+        }
+    }
+    return hit;
+}
+
+void ICacheFunctional::readLine(uint64_t adr, WayMemType *way) {
+    Axi4TransactionType tr;
+    uint32_t index = getAdrIndex(adr);
+    uint64_t tag = getAdrTag(adr);
+    int burst_steps = ICACHE_LINE_BYTES / PAYLOAD_MAX_BYTES;
+    int wstrb = 0x1;
+    tr.action = MemAction_Read;
+    tr.xsize = PAYLOAD_MAX_BYTES;
+    tr.addr = adr & ~(ICACHE_LINE_BYTES - 1);
+    tr.wstrb = 0;
+    for (int burst = 0; burst < burst_steps; burst ++) {
+        isysbus_->b_transport(&tr);
+        way->writeLine(index, tag, wstrb, tr.rpayload.b64[0]);
+
+        wstrb <<= 1;
+        tr.addr += PAYLOAD_MAX_BYTES;
+    }
+}
+
+uint64_t ICacheFunctional::getAdrLine(uint64_t adr) {
+    uint64_t mask = (1ull << (BUS_ADDR_WIDTH)) - 1;
+    uint64_t t = (adr & mask) >> OFFSET_WIDTH;
+    return t << OFFSET_WIDTH;
+}
+
+// addr[31:14]
 uint64_t ICacheFunctional::getAdrTag(uint64_t adr) {
     uint64_t mask = (1ull << (BUS_ADDR_WIDTH)) - 1;
-    uint64_t t = (adr & mask) >> (OFFSET_WIDTH + INDEX_WIDTH);
+    uint64_t t = (adr & mask) >> (OFFSET_WIDTH + ODDEVEN_WIDTH + INDEX_WIDTH);
     return t;
 }
 
+// add[13:6]
 uint32_t ICacheFunctional::getAdrIndex(uint64_t adr) {
-    uint64_t mask = (1ull << (OFFSET_WIDTH + INDEX_WIDTH)) - 1;
-    uint64_t t = (adr & mask) >> OFFSET_WIDTH;
+    uint64_t mask = (1ull << (OFFSET_WIDTH + ODDEVEN_WIDTH + INDEX_WIDTH)) - 1;
+    uint64_t t = (adr & mask) >> (OFFSET_WIDTH + ODDEVEN_WIDTH);
     return static_cast<uint32_t>(t);
 }
 
+// addr[5]
+uint64_t ICacheFunctional::getAdrOddEven(uint64_t adr) {
+    uint64_t t = (adr >> OFFSET_WIDTH) & 0x1;
+    return t;
+}
+
+// addr[4:0]
 uint32_t ICacheFunctional::getAdrOffset(uint64_t adr) {
     uint64_t mask = (1ull << OFFSET_WIDTH) - 1;
-    uint64_t t = (adr & mask);
+    uint64_t t = adr & mask;
     return static_cast<uint32_t>(t);
 }
 
@@ -243,7 +241,7 @@ void ICacheFunctional::runTest() {
     tr.wstrb = 0;
     tr.xsize = 4;
     for (unsigned i = 0; i < 100; i++) {
-        tr.addr = i << 1;     // 2-bytes alignment (uint16_t)
+        tr.addr = 0x1A + (i << 1);     // 2-bytes alignment (uint16_t)
         b_transport(&tr);
     }
 }
--- debugger/src/cpu_fnc_plugin/icache_func.h
+++ debugger/src/cpu_fnc_plugin/icache_func.h
@@ -24,6 +24,61 @@
 
 namespace debugger {
 
+static const int BUS_ADDR_WIDTH = 32;
+static const int OFFSET_WIDTH   = 5;    // [4:0]  offset: 32 bytes per line
+static const int ODDEVEN_WIDTH  = 1;    // [5]     0=even; 1=odd
+static const int INDEX_WIDTH    = 8;    // [13:6]  index: 8 KB per odd/even ways
+static const int TAG_WIDTH      = 18;   // [31:14] tag
+
+static const int ICACHE_TOTAL_BYTES = 65536;
+static const int ICACHE_LINE_BYTES  = 32;
+static const int ICACHE_WAYS        = 4;    // 4 odds, 4 even
+static const int BYTES_PER_WAY      = ICACHE_TOTAL_BYTES / (2 * ICACHE_WAYS);
+static const int LINES_PER_WAY      = BYTES_PER_WAY / ICACHE_LINE_BYTES;
+
+static const int HIT_WORD0  = 0x1;
+static const int HIT_WORD1  = 0x2;
+static const int HIT_BOTH   = HIT_WORD0 | HIT_WORD1;
+
+struct CacheLineData {
+    uint64_t tag;
+    Reg64Type mem64[ICACHE_LINE_BYTES / sizeof(uint64_t)];
+};
+
+class WayMemType {
+ public:
+    WayMemType() {}
+    ~WayMemType() {}
+
+    void writeLine(uint32_t index, uint64_t tag, int wstrb, uint64_t wdata) {
+        for (int i = 0; i < 4; i++) {
+            if (wstrb & (1 << i)) {
+                lines_[index].tag = tag;
+                lines_[index].mem64[i].val = wdata;
+            }
+        }
+    }
+    void readTagValue(uint64_t index, uint64_t off,
+                      uint64_t &tag, uint32_t &val) {
+        Reg64Type ret;
+        tag = lines_[index].tag;
+
+        // Two-bytes alignment with wrapping
+        int wcnt = (off >> 3) & 0x3;
+        int bcnt = (off >> 1) & 0x3;
+        ret.buf16[0] = lines_[index].mem64[wcnt].buf16[bcnt];
+
+        off += 2;
+        wcnt = (off >> 3) & 0x3;
+        bcnt = (off >> 1) & 0x3;
+        ret.buf16[1] = lines_[index].mem64[wcnt].buf16[bcnt];
+        val = ret.buf32[0];
+    }
+
+ private:
+    CacheLineData lines_[LINES_PER_WAY];
+};
+
 class ICacheFunctional : public IService,
                          public IMemoryOperation,
                          public ICommand {
@@ -43,9 +98,14 @@ class ICacheFunctional : public IService,
     virtual void exec(AttributeType *args, AttributeType *res);
 
  private:
+    uint64_t getAdrLine(uint64_t adr);
+    uint64_t getAdrOddEven(uint64_t adr);
     uint64_t getAdrTag(uint64_t adr);
     uint32_t getAdrIndex(uint64_t adr);
     uint32_t getAdrOffset(uint64_t adr);
+
+    int getCachedValue(uint64_t adr, uint32_t &rdata);
+    void readLine(uint64_t adr, WayMemType *way);
     void runTest();
 
  private:
@@ -69,6 +129,14 @@ class ICacheFunctional : public IService,
         LineAddressId adr[2];   // Two uint16_t words
     };
     InstructionRequestType req_;
+
+    enum EWays {
+        WAY_EVEN,
+        WAY_ODD,
+        WAY_SubNum
+    };
+    WayMemType wayOdd_[ICACHE_WAYS];
+    WayMemType wayEven_[ICACHE_WAYS];
 
     /** By default suppose using:
             16 memory banks with 16 bits width to provide C-instruction
