14222f5a35e4e8fbd998b0f55067388eec3d6ac0
sergeykhbr <sergeykhbr@gmail.com>
2022-07-12 19:19:23 +0300

    [*] Insert rtlgen generated sysc module regibank instead of manually written

--- debugger/src/cpu_sysc_plugin/riverlib/core/regibank.cpp
+++ debugger/src/cpu_sysc_plugin/riverlib/core/regibank.cpp
@@ -1,25 +1,27 @@
-/*
- *  Copyright 2019 Sergey Khabarov, sergeykhbr@gmail.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+// 
+//  Copyright 2022 Sergey Khabarov, sergeykhbr@gmail.com
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
 
 #include "regibank.h"
+#include "api_core.h"
 
 namespace debugger {
 
-RegIntBank::RegIntBank(sc_module_name name_, bool async_reset) :
-    sc_module(name_),
+RegIntBank::RegIntBank(sc_module_name name,
+                       bool async_reset)
+    : sc_module(name),
     i_clk("i_clk"),
     i_nrst("i_nrst"),
     i_radr1("i_radr1"),
@@ -41,99 +43,107 @@ RegIntBank::RegIntBank(sc_module_name name_, bool async_reset) :
     o_dport_rdata("o_dport_rdata"),
     o_ra("o_ra"),
     o_sp("o_sp") {
+
     async_reset_ = async_reset;
 
     SC_METHOD(comb);
     sensitive << i_nrst;
     sensitive << i_radr1;
     sensitive << i_radr2;
+    sensitive << i_waddr;
     sensitive << i_wena;
     sensitive << i_wtag;
     sensitive << i_wdata;
-    sensitive << i_waddr;
     sensitive << i_inorder;
+    sensitive << i_dport_addr;
     sensitive << i_dport_ena;
     sensitive << i_dport_write;
-    sensitive << i_dport_addr;
     sensitive << i_dport_wdata;
-    sensitive << r.update;
+    for (int i = 0; i < REGS_TOTAL; i++) {
+        sensitive << r.reg[i].val;
+        sensitive << r.reg[i].tag;
+    }
 
     SC_METHOD(registers);
     sensitive << i_nrst;
     sensitive << i_clk.pos();
-};
+}
 
 void RegIntBank::generateVCD(sc_trace_file *i_vcd, sc_trace_file *o_vcd) {
+    std::string pn(name());
     if (o_vcd) {
-        sc_trace(o_vcd, i_wena, i_wena.name());
+        sc_trace(o_vcd, i_radr1, i_radr1.name());
+        sc_trace(o_vcd, o_rdata1, o_rdata1.name());
+        sc_trace(o_vcd, o_rtag1, o_rtag1.name());
+        sc_trace(o_vcd, i_radr2, i_radr2.name());
+        sc_trace(o_vcd, o_rdata2, o_rdata2.name());
+        sc_trace(o_vcd, o_rtag2, o_rtag2.name());
         sc_trace(o_vcd, i_waddr, i_waddr.name());
-        sc_trace(o_vcd, i_wdata, i_wdata.name());
+        sc_trace(o_vcd, i_wena, i_wena.name());
         sc_trace(o_vcd, i_wtag, i_wtag.name());
+        sc_trace(o_vcd, i_wdata, i_wdata.name());
         sc_trace(o_vcd, i_inorder, i_inorder.name());
         sc_trace(o_vcd, o_ignored, o_ignored.name());
-        sc_trace(o_vcd, i_radr1, i_radr1.name());
-        sc_trace(o_vcd, i_radr2, i_radr2.name());
-        sc_trace(o_vcd, o_rdata1, o_rdata1.name());
-        sc_trace(o_vcd, o_rdata2, o_rdata2.name());
-        sc_trace(o_vcd, o_rtag1, o_rtag1.name());
-        sc_trace(o_vcd, o_rtag2, o_rtag2.name());
+        sc_trace(o_vcd, i_dport_addr, i_dport_addr.name());
+        sc_trace(o_vcd, i_dport_ena, i_dport_ena.name());
+        sc_trace(o_vcd, i_dport_write, i_dport_write.name());
+        sc_trace(o_vcd, i_dport_wdata, i_dport_wdata.name());
         sc_trace(o_vcd, o_dport_rdata, o_dport_rdata.name());
-
-        std::string pn(name());
-        sc_trace(o_vcd, r.reg[REG_S0].val, pn + ".s0");
-        sc_trace(o_vcd, r.reg[REG_S0].tag, pn + ".s0_tag");
-        sc_trace(o_vcd, r.reg[REG_S1].val, pn + ".s1");
-        sc_trace(o_vcd, r.reg[REG_S1].tag, pn + ".s1_tag");
-        sc_trace(o_vcd, r.reg[REG_A5].val, pn + ".a5");
-        sc_trace(o_vcd, r.reg[REG_A5].tag, pn + ".a5_tag");
-        sc_trace(o_vcd, r.reg[REG_TP].val, pn + ".tp");
-        sc_trace(o_vcd, r.reg[REG_TP].tag, pn + ".tp_tag");
-        sc_trace(o_vcd, r.reg[REG_SP].val, pn + ".sp");
-        sc_trace(o_vcd, r.reg[REG_SP].tag, pn + ".sp_tag");
+        sc_trace(o_vcd, o_ra, o_ra.name());
+        sc_trace(o_vcd, o_sp, o_sp.name());
+        for (int i = 0; i < REGS_TOTAL; i++) {
+            char tstr[1024];
+            RISCV_sprintf(tstr, sizeof(tstr), "%s.r_reg%d_val", pn.c_str(), i);
+            sc_trace(o_vcd, r.reg[i].val, tstr);
+            RISCV_sprintf(tstr, sizeof(tstr), "%s.r_reg%d_tag", pn.c_str(), i);
+            sc_trace(o_vcd, r.reg[i].tag, tstr);
+        }
     }
+
 }
 
 void RegIntBank::comb() {
-    int int_daddr = i_dport_addr.read().to_int();
-    int int_waddr = i_waddr.read().to_int();
-    int int_radr1 = i_radr1.read().to_int();
-    int int_radr2 = i_radr2.read().to_int();
+    int int_daddr;
+    int int_waddr;
+    int int_radr1;
+    int int_radr2;
     bool v_inordered;
     sc_uint<CFG_REG_TAG_WIDTH> next_tag;
 
-    v = r;
+    for (int i = 0; i < REGS_TOTAL; i++) {
+        v.reg[i].val = r.reg[i].val;
+        v.reg[i].tag = r.reg[i].tag;
+    }
 
     v_inordered = 0;
+    int_daddr = i_dport_addr.read().to_int();
+    int_waddr = i_waddr.read().to_int();
+    int_radr1 = i_radr1.read().to_int();
+    int_radr2 = i_radr2.read().to_int();
 
-    next_tag = r.reg[int_waddr].tag + 1;
+    next_tag = (r.reg[int_waddr].tag.read() + 1);
     if (next_tag == i_wtag.read()) {
         v_inordered = 1;
     }
 
-    /** Debug port has lower priority to avoid system hangup due the tags error */
-    if (i_wena.read() == 1 && i_waddr.read().or_reduce() == 1
-        && (!i_inorder.read() || v_inordered)) {
+    // Debug port has lower priority to avoid system hangup due the tags error
+    if ((i_wena.read() == 1) && (i_waddr.read().or_reduce() == 1) && ((!i_inorder) || v_inordered)) {
         v.reg[int_waddr].val = i_wdata;
         v.reg[int_waddr].tag = i_wtag;
-    } else if (i_dport_ena.read() && i_dport_write.read()) {
+    } else if (i_dport_ena && i_dport_write) {
         if (i_dport_addr.read().or_reduce() == 1) {
             v.reg[int_daddr].val = i_dport_wdata;
         }
     }
-    v.update = !r.update.read();
 
-    if (!async_reset_ && !i_nrst.read()) {   
-        v.reg[0].val = 0;
-        v.reg[0].tag = 0;
-        for (int i = 1; i < REGS_TOTAL; i++) {
-            v.reg[i].val = 0xfeedface;
+    if (!async_reset_ && i_nrst.read() == 0) {
+        for (int i = 0; i < REGS_TOTAL; i++) {
+            v.reg[i].val = 0;
             v.reg[i].tag = 0;
         }
-        v.update = 0;
     }
 
-    o_ignored = i_wena && i_waddr.read().or_reduce() && i_inorder && !v_inordered;
-
+    o_ignored = (i_wena && (i_waddr.read().or_reduce() == 1) && i_inorder && (!v_inordered));
     o_rdata1 = r.reg[int_radr1].val;
     o_rtag1 = r.reg[int_radr1].tag;
     o_rdata2 = r.reg[int_radr2].val;
@@ -145,15 +155,15 @@ void RegIntBank::comb() {
 
 void RegIntBank::registers() {
     if (async_reset_ && i_nrst.read() == 0) {
-        r.reg[0].val = 0;
-        r.reg[0].tag = 0;
-        for (int i = 1; i < REGS_TOTAL; i++) {
-            r.reg[i].val = 0xfeedface;
+        for (int i = 0; i < REGS_TOTAL; i++) {
+            r.reg[i].val = 0;
             r.reg[i].tag = 0;
         }
-        r.update = 0;
     } else {
-        r = v;
+        for (int i = 0; i < REGS_TOTAL; i++) {
+            r.reg[i].val = v.reg[i].val;
+            r.reg[i].tag = v.reg[i].tag;
+        }
     }
 }
 
--- debugger/src/cpu_sysc_plugin/riverlib/core/regibank.h
+++ debugger/src/cpu_sysc_plugin/riverlib/core/regibank.h
@@ -1,21 +1,19 @@
-/*
- *  Copyright 2019 Sergey Khabarov, sergeykhbr@gmail.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
-#ifndef __DEBUGGER_RIVERLIB_REGIBANK_H__
-#define __DEBUGGER_RIVERLIB_REGIBANK_H__
+// 
+//  Copyright 2022 Sergey Khabarov, sergeykhbr@gmail.com
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+#pragma once
 
 #include <systemc.h>
 #include "../river_cfg.h"
@@ -23,57 +21,54 @@
 namespace debugger {
 
 SC_MODULE(RegIntBank) {
-    sc_in<bool> i_clk;                          // Clock
-    sc_in<bool> i_nrst;                         // Reset. Active LOW
-    sc_in<sc_uint<6>> i_radr1;                  // Port 1 read address
-    sc_out<sc_uint<RISCV_ARCH>> o_rdata1;       // Port 1 read value
-    sc_out<sc_uint<CFG_REG_TAG_WIDTH>> o_rtag1;  // Port 1 read tag value
-
-    sc_in<sc_uint<6>> i_radr2;                  // Port 2 read address
-    sc_out<sc_uint<RISCV_ARCH>> o_rdata2;       // Port 2 read value
-    sc_out<sc_uint<CFG_REG_TAG_WIDTH>> o_rtag2;  // Port 2 read tag value
-
-    sc_in<sc_uint<6>> i_waddr;                  // Writing value
-    sc_in<bool> i_wena;                         // Writing is enabled
-    sc_in<sc_uint<CFG_REG_TAG_WIDTH>> i_wtag;    // Writing register tag
-    sc_in<sc_uint<RISCV_ARCH>> i_wdata;         // Writing value
-    sc_in<bool> i_inorder;                      // Writing only if tag sequenced
-    sc_out<bool> o_ignored;                     // Sequenced writing is ignored because it was overwritten by executor (need for tracer)
-
-    sc_in<sc_uint<6>> i_dport_addr;             // Debug port address
-    sc_in<bool> i_dport_ena;                    // Debug port is enabled
-    sc_in<bool> i_dport_write;                  // Debug port write is enabled
-    sc_in<sc_uint<RISCV_ARCH>> i_dport_wdata;   // Debug port write value
-    sc_out<sc_uint<RISCV_ARCH>> o_dport_rdata;  // Debug port read value
-
-    sc_out<sc_uint<RISCV_ARCH>> o_ra;       // Return address for branch predictor
-    sc_out<sc_uint<RISCV_ARCH>> o_sp;       // Stack Pointer for border control
+ public:
+    sc_in<bool> i_clk;                                      // CPU clock
+    sc_in<bool> i_nrst;                                     // Reset: active LOW
+    sc_in<sc_uint<6>> i_radr1;                              // Port 1 read address
+    sc_out<sc_uint<RISCV_ARCH>> o_rdata1;                   // Port 1 read value
+    sc_out<sc_uint<CFG_REG_TAG_WIDTH>> o_rtag1;             // Port 1 read tag value
+    sc_in<sc_uint<6>> i_radr2;                              // Port 2 read address
+    sc_out<sc_uint<RISCV_ARCH>> o_rdata2;                   // Port 2 read value
+    sc_out<sc_uint<CFG_REG_TAG_WIDTH>> o_rtag2;             // Port 2 read tag value
+    sc_in<sc_uint<6>> i_waddr;                              // Writing value
+    sc_in<bool> i_wena;                                     // Writing is enabled
+    sc_in<sc_uint<CFG_REG_TAG_WIDTH>> i_wtag;               // Writing register tag
+    sc_in<sc_uint<RISCV_ARCH>> i_wdata;                     // Writing value
+    sc_in<bool> i_inorder;                                  // Writing only if tag sequenced
+    sc_out<bool> o_ignored;                                 // Sequenced writing is ignored because it was overwritten by executor (need for tracer)
+    sc_in<sc_uint<6>> i_dport_addr;                         // Debug port address
+    sc_in<bool> i_dport_ena;                                // Debug port is enabled
+    sc_in<bool> i_dport_write;                              // Debug port write is enabled
+    sc_in<sc_uint<RISCV_ARCH>> i_dport_wdata;               // Debug port write value
+    sc_out<sc_uint<RISCV_ARCH>> o_dport_rdata;              // Debug port read value
+    sc_out<sc_uint<RISCV_ARCH>> o_ra;                       // Return address for branch predictor
+    sc_out<sc_uint<RISCV_ARCH>> o_sp;                       // Stack Pointer for border control
 
     void comb();
     void registers();
 
     SC_HAS_PROCESS(RegIntBank);
 
-    RegIntBank(sc_module_name name_, bool async_reset);
+    RegIntBank(sc_module_name name,
+               bool async_reset);
 
     void generateVCD(sc_trace_file *i_vcd, sc_trace_file *o_vcd);
 
-private:
-    friend struct Processor; // for debug purposes(remove it)s
+ private:
+    bool async_reset_;
 
-    struct reg_score_type {
-        sc_uint<RISCV_ARCH> val;
-        sc_uint<CFG_REG_TAG_WIDTH> tag;
+    struct RegValueType {
+        sc_signal<sc_uint<RISCV_ARCH>> val;
+        sc_signal<sc_uint<CFG_REG_TAG_WIDTH>> tag;
     };
 
-    struct RegistersType {
-        sc_signal<bool> update;             // To generate SystemC delta event only.
-        reg_score_type reg[REGS_TOTAL]; // Multi-ports memory
+
+    struct RegIntBank_registers {
+        RegValueType reg[REGS_TOTAL];
     } v, r;
 
-    bool async_reset_;
+
 };
 
 }  // namespace debugger
 
-#endif  // __DEBUGGER_RIVERLIB_REGIBANK_H__
