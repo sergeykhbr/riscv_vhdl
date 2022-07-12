4340a522b58f8d60a845e88bb4c5b146a16a8190
sergeykhbr <sergeykhbr@gmail.com>
2022-07-12 23:00:06 +0300

    [*] fetch systemc module inserted from rtlgen generator

--- debugger/src/cpu_sysc_plugin/riverlib/core/fetch.cpp
+++ debugger/src/cpu_sysc_plugin/riverlib/core/fetch.cpp
@@ -1,25 +1,27 @@
-/*
- *  Copyright 2019 Sergey Khabarov, sergeykhbr@gmail.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+// 
+//  Copyright 2022 Sergey Khabarov, sergeykhbr@gmail.com
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
 
 #include "fetch.h"
+#include "api_core.h"
 
 namespace debugger {
 
-InstrFetch::InstrFetch(sc_module_name name_, bool async_reset) :
-    sc_module(name_),
+InstrFetch::InstrFetch(sc_module_name name,
+                       bool async_reset)
+    : sc_module(name),
     i_clk("i_clk"),
     i_nrst("i_nrst"),
     i_bp_valid("i_bp_valid"),
@@ -43,6 +45,7 @@ InstrFetch::InstrFetch(sc_module_name name_, bool async_reset) :
     o_instr_executable("o_instr_executable"),
     o_pc("o_pc"),
     o_instr("o_instr") {
+
     async_reset_ = async_reset;
 
     SC_METHOD(comb);
@@ -50,8 +53,8 @@ InstrFetch::InstrFetch(sc_module_name name_, bool async_reset) :
     sensitive << i_bp_valid;
     sensitive << i_bp_pc;
     sensitive << i_mem_req_ready;
-    sensitive << i_mem_data_addr;
     sensitive << i_mem_data_valid;
+    sensitive << i_mem_data_addr;
     sensitive << i_mem_data;
     sensitive << i_mem_load_fault;
     sensitive << i_mem_executable;
@@ -73,65 +76,70 @@ InstrFetch::InstrFetch(sc_module_name name_, bool async_reset) :
     SC_METHOD(registers);
     sensitive << i_nrst;
     sensitive << i_clk.pos();
-};
+}
 
 void InstrFetch::generateVCD(sc_trace_file *i_vcd, sc_trace_file *o_vcd) {
+    std::string pn(name());
     if (o_vcd) {
+        sc_trace(o_vcd, i_bp_valid, i_bp_valid.name());
+        sc_trace(o_vcd, i_bp_pc, i_bp_pc.name());
+        sc_trace(o_vcd, o_requested_pc, o_requested_pc.name());
+        sc_trace(o_vcd, o_fetching_pc, o_fetching_pc.name());
+        sc_trace(o_vcd, i_mem_req_ready, i_mem_req_ready.name());
+        sc_trace(o_vcd, o_mem_addr_valid, o_mem_addr_valid.name());
+        sc_trace(o_vcd, o_mem_addr, o_mem_addr.name());
         sc_trace(o_vcd, i_mem_data_valid, i_mem_data_valid.name());
         sc_trace(o_vcd, i_mem_data_addr, i_mem_data_addr.name());
         sc_trace(o_vcd, i_mem_data, i_mem_data.name());
         sc_trace(o_vcd, i_mem_load_fault, i_mem_load_fault.name());
         sc_trace(o_vcd, i_mem_executable, i_mem_executable.name());
+        sc_trace(o_vcd, o_mem_resp_ready, o_mem_resp_ready.name());
         sc_trace(o_vcd, i_flush_pipeline, i_flush_pipeline.name());
         sc_trace(o_vcd, i_progbuf_ena, i_progbuf_ena.name());
         sc_trace(o_vcd, i_progbuf_pc, i_progbuf_pc.name());
         sc_trace(o_vcd, i_progbuf_instr, i_progbuf_instr.name());
-        sc_trace(o_vcd, o_mem_resp_ready, o_mem_resp_ready.name());
-        sc_trace(o_vcd, i_bp_pc, i_bp_pc.name());
-        sc_trace(o_vcd, i_bp_valid, i_bp_valid.name());
-        sc_trace(o_vcd, o_mem_addr_valid, o_mem_addr_valid.name());
-        sc_trace(o_vcd, o_mem_addr, o_mem_addr.name());
-        sc_trace(o_vcd, i_mem_req_ready, i_mem_req_ready.name());
-        sc_trace(o_vcd, o_requested_pc, o_requested_pc.name());
-        sc_trace(o_vcd, o_fetching_pc, o_fetching_pc.name());
-        sc_trace(o_vcd, o_pc, o_pc.name());
-        sc_trace(o_vcd, o_instr, o_instr.name());
         sc_trace(o_vcd, o_instr_load_fault, o_instr_load_fault.name());
         sc_trace(o_vcd, o_instr_executable, o_instr_executable.name());
-
-        std::string pn(name());
+        sc_trace(o_vcd, o_pc, o_pc.name());
+        sc_trace(o_vcd, o_instr, o_instr.name());
         sc_trace(o_vcd, r.state, pn + ".r_state");
         sc_trace(o_vcd, r.req_valid, pn + ".r_req_valid");
+        sc_trace(o_vcd, r.resp_ready, pn + ".r_resp_ready");
         sc_trace(o_vcd, r.req_addr, pn + ".r_req_addr");
         sc_trace(o_vcd, r.mem_resp_shadow, pn + ".r_mem_resp_shadow");
+        sc_trace(o_vcd, r.pc, pn + ".r_pc");
+        sc_trace(o_vcd, r.instr, pn + ".r_instr");
+        sc_trace(o_vcd, r.instr_load_fault, pn + ".r_instr_load_fault");
+        sc_trace(o_vcd, r.instr_executable, pn + ".r_instr_executable");
+        sc_trace(o_vcd, r.progbuf_ena, pn + ".r_progbuf_ena");
     }
+
 }
 
 void InstrFetch::comb() {
-
     v = r;
 
     switch (r.state.read()) {
-    case Idle:
+    case Idle :
         v.req_valid = 0;
         v.resp_ready = 0;
         v.progbuf_ena = 0;
-        if (i_progbuf_ena.read()) {
+        if (i_progbuf_ena.read() == 1) {
             // Execution from buffer
             v.progbuf_ena = 1;
-            v.pc = i_progbuf_pc.read();
+            v.pc = i_progbuf_pc;
             v.instr = i_progbuf_instr;
             v.instr_load_fault = 0;
             v.instr_executable = 0;
-        } else if (i_bp_valid) {
+        } else if (i_bp_valid.read() == 1) {
             v.state = WaitReqAccept;
             v.req_addr = i_bp_pc;
             v.req_valid = 1;
         }
         break;
-    case WaitReqAccept:
+    case WaitReqAccept :
         if (i_mem_req_ready) {
-            v.req_valid = i_bp_valid && !i_progbuf_ena;
+            v.req_valid = (i_bp_valid && (!i_progbuf_ena));
             v.req_addr = i_bp_pc;
             v.mem_resp_shadow = r.req_addr;
             v.resp_ready = 1;
@@ -141,22 +149,22 @@ void InstrFetch::comb() {
             v.req_addr = i_bp_pc;
         }
         break;
-    case WaitResp:
-        if (i_mem_data_valid) {
+    case WaitResp :
+        if (i_mem_data_valid.read() == 1) {
             v.pc = i_mem_data_addr;
             v.instr = i_mem_data;
-            v.instr_load_fault = i_mem_load_fault.read();
-            v.instr_executable = i_mem_executable.read();
-            v.req_valid = i_bp_valid && !i_progbuf_ena;
+            v.instr_load_fault = i_mem_load_fault;
+            v.instr_executable = i_mem_executable;
+            v.req_valid = (i_bp_valid && (!i_progbuf_ena));
 
-            if (r.req_valid) {
-                if (i_mem_req_ready) {
+            if (r.req_valid.read() == 1) {
+                if (i_mem_req_ready.read() == 1) {
                     v.req_addr = i_bp_pc;
                     v.mem_resp_shadow = r.req_addr;
                 } else {
                     v.state = WaitReqAccept;
                 }
-            } else if (i_bp_valid && !i_progbuf_ena) {
+            } else if (i_bp_valid && (!i_progbuf_ena)) {
                 v.req_addr = i_bp_pc;
                 v.state = WaitReqAccept;
             } else {
@@ -165,7 +173,8 @@ void InstrFetch::comb() {
             }
         }
         break;
-    default:;
+    default:
+        break;
     }
 
     if (i_flush_pipeline.read() == 1) {
@@ -177,8 +186,8 @@ void InstrFetch::comb() {
         v.instr_executable = 0;
     }
 
-    if (!async_reset_ && !i_nrst.read()) {
-        R_RESET(v);
+    if (!async_reset_ && i_nrst.read() == 0) {
+        InstrFetch_r_reset(v);
     }
 
     o_mem_addr_valid = r.req_valid;
@@ -194,7 +203,7 @@ void InstrFetch::comb() {
 
 void InstrFetch::registers() {
     if (async_reset_ && i_nrst.read() == 0) {
-        R_RESET(r);
+        InstrFetch_r_reset(r);
     } else {
         r = v;
     }
--- debugger/src/cpu_sysc_plugin/riverlib/core/fetch.h
+++ debugger/src/cpu_sysc_plugin/riverlib/core/fetch.h
@@ -1,21 +1,19 @@
-/*
- *  Copyright 2019 Sergey Khabarov, sergeykhbr@gmail.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
-#ifndef __DEBUGGER_RIVERLIB_FETCH_H__
-#define __DEBUGGER_RIVERLIB_FETCH_H__
+// 
+//  Copyright 2022 Sergey Khabarov, sergeykhbr@gmail.com
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+#pragma once
 
 #include <systemc.h>
 #include "../river_cfg.h"
@@ -23,8 +21,9 @@
 namespace debugger {
 
 SC_MODULE(InstrFetch) {
-    sc_in<bool> i_clk;
-    sc_in<bool> i_nrst;
+ public:
+    sc_in<bool> i_clk;                                      // CPU clock
+    sc_in<bool> i_nrst;                                     // Reset: active LOW
     sc_in<bool> i_bp_valid;
     sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_bp_pc;
     sc_out<sc_uint<CFG_CPU_ADDR_BITS>> o_requested_pc;
@@ -38,12 +37,10 @@ SC_MODULE(InstrFetch) {
     sc_in<bool> i_mem_load_fault;
     sc_in<bool> i_mem_executable;
     sc_out<bool> o_mem_resp_ready;
-
-    sc_in<bool> i_flush_pipeline;                   // reset pipeline and cache
-    sc_in<bool> i_progbuf_ena;                      // executing from prog buffer
-    sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_progbuf_pc; // progbuf counter
-    sc_in<sc_uint<64>> i_progbuf_instr;             // progbuf instruction
-
+    sc_in<bool> i_flush_pipeline;                           // reset pipeline and cache
+    sc_in<bool> i_progbuf_ena;                              // executing from prog buffer
+    sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_progbuf_pc;         // progbuf counter
+    sc_in<sc_uint<64>> i_progbuf_instr;                     // progbuf instruction
     sc_out<bool> o_instr_load_fault;
     sc_out<bool> o_instr_executable;
     sc_out<sc_uint<CFG_CPU_ADDR_BITS>> o_pc;
@@ -54,21 +51,24 @@ SC_MODULE(InstrFetch) {
 
     SC_HAS_PROCESS(InstrFetch);
 
-    InstrFetch(sc_module_name name_, bool async_reset);
+    InstrFetch(sc_module_name name,
+               bool async_reset);
 
     void generateVCD(sc_trace_file *i_vcd, sc_trace_file *o_vcd);
 
  private:
-    static const uint8_t Idle = 0;
-    static const uint8_t WaitReqAccept = 1;
-    static const uint8_t WaitResp = 2;
+    bool async_reset_;
 
-    struct RegistersType {
+    static const uint32_t Idle = 0;
+    static const uint32_t WaitReqAccept = 1;
+    static const uint32_t WaitResp = 2;
+
+    struct InstrFetch_registers {
         sc_signal<sc_uint<2>> state;
         sc_signal<bool> req_valid;
         sc_signal<bool> resp_ready;
         sc_signal<sc_uint<CFG_CPU_ADDR_BITS>> req_addr;
-        sc_signal<sc_uint<CFG_CPU_ADDR_BITS>> mem_resp_shadow;  // the same as memory response but internal
+        sc_signal<sc_uint<CFG_CPU_ADDR_BITS>> mem_resp_shadow;// the same as memory response but internal
         sc_signal<sc_uint<CFG_CPU_ADDR_BITS>> pc;
         sc_signal<sc_uint<64>> instr;
         sc_signal<bool> instr_load_fault;
@@ -76,7 +76,7 @@ SC_MODULE(InstrFetch) {
         sc_signal<bool> progbuf_ena;
     } v, r;
 
-    void R_RESET(RegistersType &iv) {
+    void InstrFetch_r_reset(InstrFetch_registers &iv) {
         iv.state = Idle;
         iv.req_valid = 0;
         iv.resp_ready = 0;
@@ -89,10 +89,7 @@ SC_MODULE(InstrFetch) {
         iv.progbuf_ena = 0;
     }
 
-    bool async_reset_;
 };
-
 
 }  // namespace debugger
 
-#endif  // __DEBUGGER_RIVERLIB_FETCH_H__
