de4627fae368ea61f91b70b1c1077b65963ecc3c
sergeykhbr <sergeykhbr@gmail.com>
2022-09-02 12:03:02 +0300

    [+] Use MPU rtkgen generated module and fix bootrom_test final hex-image size to avoid trimming

--- debugger/src/cpu_sysc_plugin/riverlib/cache/mpu.cpp
+++ debugger/src/cpu_sysc_plugin/riverlib/cache/mpu.cpp
@@ -1,25 +1,27 @@
-/*
- *  Copyright 2019 Sergey Khabarov, sergeykhbr@gmail.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+// 
+//  Copyright 2022 Sergey Khabarov, sergeykhbr@gmail.com
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
 
 #include "mpu.h"
-#include <api_core.h>
+#include "api_core.h"
 
 namespace debugger {
 
-MPU::MPU(sc_module_name name_, bool async_reset) : sc_module(name_) ,
+MPU::MPU(sc_module_name name,
+         bool async_reset)
+    : sc_module(name),
     i_clk("i_clk"),
     i_nrst("i_nrst"),
     i_iaddr("i_iaddr"),
@@ -31,6 +33,7 @@ MPU::MPU(sc_module_name name_, bool async_reset) : sc_module(name_) ,
     i_region_flags("i_region_flags"),
     o_iflags("o_iflags"),
     o_dflags("o_dflags") {
+
     async_reset_ = async_reset;
 
     SC_METHOD(comb);
@@ -42,105 +45,93 @@ MPU::MPU(sc_module_name name_, bool async_reset) : sc_module(name_) ,
     sensitive << i_region_addr;
     sensitive << i_region_mask;
     sensitive << i_region_flags;
+    for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
+        sensitive << r.tbl[i].addr;
+        sensitive << r.tbl[i].mask;
+        sensitive << r.tbl[i].flags;
+    }
 
     SC_METHOD(registers);
+    sensitive << i_nrst;
     sensitive << i_clk.pos();
-};
+}
 
 void MPU::generateVCD(sc_trace_file *i_vcd, sc_trace_file *o_vcd) {
+    std::string pn(name());
     if (o_vcd) {
         sc_trace(o_vcd, i_iaddr, i_iaddr.name());
         sc_trace(o_vcd, i_daddr, i_daddr.name());
         sc_trace(o_vcd, i_region_we, i_region_we.name());
         sc_trace(o_vcd, i_region_idx, i_region_idx.name());
+        sc_trace(o_vcd, i_region_addr, i_region_addr.name());
+        sc_trace(o_vcd, i_region_mask, i_region_mask.name());
+        sc_trace(o_vcd, i_region_flags, i_region_flags.name());
         sc_trace(o_vcd, o_iflags, o_iflags.name());
         sc_trace(o_vcd, o_dflags, o_dflags.name());
+        for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
+            char tstr[1024];
+            RISCV_sprintf(tstr, sizeof(tstr), "%s.r_tbl%d_flags", pn.c_str(), i);
+            sc_trace(o_vcd, r.tbl[i].flags, tstr);
+        }
     }
+
 }
 
 void MPU::comb() {
-    MpuTableItemType v_item;
     sc_uint<CFG_MPU_FL_TOTAL> v_iflags;
     sc_uint<CFG_MPU_FL_TOTAL> v_dflags;
+    sc_uint<CFG_CPU_ADDR_BITS> vb_addr;
+    sc_uint<CFG_CPU_ADDR_BITS> vb_mask;
+    sc_uint<CFG_MPU_FL_TOTAL> vb_flags;
+
+    v_iflags = 0;
+    v_dflags = 0;
+    vb_addr = 0;
+    vb_mask = 0;
+    vb_flags = 0;
 
     for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
-        v_tbl[i] = tbl[i];
+        v.tbl[i].addr = r.tbl[i].addr;
+        v.tbl[i].mask = r.tbl[i].mask;
+        v.tbl[i].flags = r.tbl[i].flags;
     }
-    v_iflags = ~0ul;
-    v_dflags = ~0ul;
 
-    v_item.flags = i_region_flags.read();
+
+    vb_flags = i_region_flags;
     if (i_region_flags.read()[CFG_MPU_FL_ENA] == 1) {
-        v_item.addr = i_region_addr.read();
-        v_item.mask = i_region_mask.read();
+        vb_addr = i_region_addr;
+        vb_mask = i_region_mask;
     } else {
-        v_item.addr = ~0ull;
-        v_item.mask = ~0ull;
+        vb_addr = ~0ull;
+        vb_mask = ~0ull;
     }
 
     for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
-        if (tbl[i].addr == (i_iaddr.read() & tbl[i].mask)) {
-            if (tbl[i].flags[CFG_MPU_FL_ENA] == 1)  {
-                v_iflags = tbl[i].flags;
+        if (r.tbl[i].addr.read() == (i_iaddr.read() & r.tbl[i].mask.read())) {
+            if (r.tbl[i].flags.read()[CFG_MPU_FL_ENA] == 1) {
+                v_iflags = r.tbl[i].flags;
             }
         }
 
-        if (tbl[i].addr == (i_daddr.read() & tbl[i].mask)) {
-            if (tbl[i].flags[CFG_MPU_FL_ENA] == 1)  {
-                v_dflags = tbl[i].flags;
+        if (r.tbl[i].addr.read() == (i_daddr.read() & r.tbl[i].mask.read())) {
+            if (r.tbl[i].flags.read()[CFG_MPU_FL_ENA] == 1) {
+                v_dflags = r.tbl[i].flags;
             }
         }
     }
 
     if (i_region_we.read() == 1) {
-        v_tbl[i_region_idx.read().to_int()] = v_item;
+        v.tbl[i_region_idx.read().to_int()].addr = vb_addr;
+        v.tbl[i_region_idx.read().to_int()].mask = vb_mask;
+        v.tbl[i_region_idx.read().to_int()].flags = vb_flags;
     }
-
 
     if (!async_reset_ && i_nrst.read() == 0) {
         for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
-            v_tbl[i].flags = 0;
-            v_tbl[i].addr = 0;
-            v_tbl[i].mask = ~0ull;
+            v.tbl[i].addr = 0ull;
+            v.tbl[i].mask = 0ull;
+            v.tbl[i].flags = ~0ul;
         }
-
-        // Uncached CLINT
-        v_tbl[0].addr = 0x0000000002000000ull;
-        v_tbl[0].mask = 0xFFFFFFFFFE000000ull;
-        v_tbl[0].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[0].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[0].flags[CFG_MPU_FL_EXEC] = 0;
-        v_tbl[0].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[0].flags[CFG_MPU_FL_WR] = 1;
-
-        // Uncached PLIC
-        v_tbl[1].addr = 0x000000000C000000ull;
-        v_tbl[1].mask = 0xFFFFFFFFFC000000ull;
-        v_tbl[1].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[1].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[1].flags[CFG_MPU_FL_EXEC] = 0;
-        v_tbl[1].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[1].flags[CFG_MPU_FL_WR] = 1;
-
-        // Uncached peripheries (IO)
-        v_tbl[2].addr = 0x0000000010000000ull;
-        v_tbl[2].mask = 0xFFFFFFFFF0000000ull;
-        v_tbl[2].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[2].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[2].flags[CFG_MPU_FL_EXEC] = 1;
-        v_tbl[2].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[2].flags[CFG_MPU_FL_WR] = 1;
-
-#if 1
-        // Debug: Make first 128 Byte uncachable to test MPU
-        v_tbl[3].addr(31, 0) = 0x00010000ull;
-        v_tbl[3].mask(31, 0) = 0xFFFFFF80ull;
-        v_tbl[3].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[3].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[3].flags[CFG_MPU_FL_EXEC] = 1;
-        v_tbl[3].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[3].flags[CFG_MPU_FL_WR] = 1;
-#endif
     }
 
     o_iflags = v_iflags;
@@ -149,41 +140,16 @@ void MPU::comb() {
 
 void MPU::registers() {
     if (async_reset_ && i_nrst.read() == 0) {
-        for (int i = 1; i < CFG_MPU_TBL_SIZE; i++) {
-            tbl[i].flags = 0;
-            tbl[i].addr = 0;
-            tbl[i].mask = ~0ull;
+        for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
+            r.tbl[i].addr = 0ull;
+            r.tbl[i].mask = 0ull;
+            r.tbl[i].flags = ~0ul;
         }
-
-        // Uncached CLINT
-        v_tbl[0].addr = 0x0000000002000000ull;
-        v_tbl[0].mask = 0xFFFFFFFFFE000000ull;
-        v_tbl[0].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[0].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[0].flags[CFG_MPU_FL_EXEC] = 0;
-        v_tbl[0].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[0].flags[CFG_MPU_FL_WR] = 1;
-
-        // Uncached PLIC
-        v_tbl[1].addr = 0x000000000C000000ull;
-        v_tbl[1].mask = 0xFFFFFFFFFC000000ull;
-        v_tbl[1].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[1].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[1].flags[CFG_MPU_FL_EXEC] = 0;
-        v_tbl[1].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[1].flags[CFG_MPU_FL_WR] = 1;
-
-        // Uncached peripheries (IO)
-        v_tbl[2].addr = 0x0000000010000000ull;
-        v_tbl[2].mask = 0xFFFFFFFFF0000000ull;
-        v_tbl[2].flags[CFG_MPU_FL_ENA] = 1;
-        v_tbl[2].flags[CFG_MPU_FL_CACHABLE] = 0;
-        v_tbl[2].flags[CFG_MPU_FL_EXEC] = 1;
-        v_tbl[2].flags[CFG_MPU_FL_RD] = 1;
-        v_tbl[2].flags[CFG_MPU_FL_WR] = 1;
     } else {
         for (int i = 0; i < CFG_MPU_TBL_SIZE; i++) {
-            tbl[i] = v_tbl[i];
+            r.tbl[i].addr = v.tbl[i].addr;
+            r.tbl[i].mask = v.tbl[i].mask;
+            r.tbl[i].flags = v.tbl[i].flags;
         }
     }
 }
--- debugger/src/cpu_sysc_plugin/riverlib/cache/mpu.h
+++ debugger/src/cpu_sysc_plugin/riverlib/cache/mpu.h
@@ -1,21 +1,19 @@
-/*
- *  Copyright 2019 Sergey Khabarov, sergeykhbr@gmail.com
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
-#ifndef __DEBUGGER_RIVERLIB_CACHE_MPU_H__
-#define __DEBUGGER_RIVERLIB_CACHE_MPU_H__
+// 
+//  Copyright 2022 Sergey Khabarov, sergeykhbr@gmail.com
+// 
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// 
+#pragma once
 
 #include <systemc.h>
 #include "../river_cfg.h"
@@ -23,15 +21,16 @@
 namespace debugger {
 
 SC_MODULE(MPU) {
-    sc_in<bool> i_clk;
-    sc_in<bool> i_nrst;
+ public:
+    sc_in<bool> i_clk;                                      // CPU clock
+    sc_in<bool> i_nrst;                                     // Reset: active LOW
     sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_iaddr;
     sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_daddr;
     sc_in<bool> i_region_we;
     sc_in<sc_uint<CFG_MPU_TBL_WIDTH>> i_region_idx;
     sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_region_addr;
     sc_in<sc_uint<CFG_CPU_ADDR_BITS>> i_region_mask;
-    sc_in<sc_uint<CFG_MPU_FL_TOTAL>> i_region_flags;  // {ena, cachable, r, w, x}
+    sc_in<sc_uint<CFG_MPU_FL_TOTAL>> i_region_flags;        // {ena, cachable, r, w, x}
     sc_out<sc_uint<CFG_MPU_FL_TOTAL>> o_iflags;
     sc_out<sc_uint<CFG_MPU_FL_TOTAL>> o_dflags;
 
@@ -40,23 +39,27 @@ SC_MODULE(MPU) {
 
     SC_HAS_PROCESS(MPU);
 
-    MPU(sc_module_name name_, bool async_reset);
+    MPU(sc_module_name name,
+        bool async_reset);
 
     void generateVCD(sc_trace_file *i_vcd, sc_trace_file *o_vcd);
 
  private:
+    bool async_reset_;
+
     struct MpuTableItemType {
-        sc_uint<CFG_CPU_ADDR_BITS> addr;
-        sc_uint<CFG_CPU_ADDR_BITS> mask;
-        sc_uint<CFG_MPU_FL_TOTAL> flags;
+        sc_signal<sc_uint<CFG_CPU_ADDR_BITS>> addr;
+        sc_signal<sc_uint<CFG_CPU_ADDR_BITS>> mask;
+        sc_signal<sc_uint<CFG_MPU_FL_TOTAL>> flags;         // Enable all regions by default
     };
 
-    MpuTableItemType tbl[CFG_MPU_TBL_SIZE];
-    MpuTableItemType v_tbl[CFG_MPU_TBL_SIZE];
 
-    bool async_reset_;
+    struct MPU_registers {
+        MpuTableItemType tbl[CFG_MPU_TBL_SIZE];
+    } v, r;
+
+
 };
 
 }  // namespace debugger
 
-#endif  // __DEBUGGER_RIVERLIB_CACHE_MPU_H__
